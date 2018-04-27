00ece23098e6c9b13423eb4e2994def8a2773156
Kate Preston <ktweeden@googlemail.com>
2018-04-27 15:02:26 +0100

    created view to populate selection dropdowns

---
+++ client/src/views/all_countries_view.js
@@ -0,0 +1,25 @@
+// populate  both selects
+
+const AllCountriesView = function() {
+
+}
+
+AllCountriesView.prototype.renderSelects = function (countriesArray) {
+  const visitedSelect = document.querySelector('#visited-select');
+  const toVisitSelect = document.querySelector('#to-visit-select');
+
+  countriesArray.forEach(country => {
+    renderOption(visitedSelect, country);
+    renderOption(toVisitSelect, country);
+  });
+};
+
+
+const renderOption = function(parentSelect, country) {
+  const option = document.createElement('option');
+  option.textContent = country.name;
+  option.value = country.alpha3code;
+  parentSelect.appendChild(parentSelect);
+};
+
+module.exports = AllCountriesView;
---
+++ client/src/views/to_visit_view.js
@@ -0,0 +1,1 @@
+// populate list of countries to visit
