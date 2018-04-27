8ec0f5a28217a13d827c5cee3c203b46c655460b
Graham Forrester <graham_forrester1@hotmail.com>
2018-04-27 11:22:35 +0100

    Setup server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log("HELLOOOOO!!!!!!!");
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+
+server.use(parser.json());
+server.use(express.static('client/public'));
+server.use(require('./controllers/index_controller.js'));
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.error(err);
+    return;
+  };
+  const db = client.db('travel_dashboard');
+  console.log('connected to DB');
+  const toVisitCountriesCollection = db.collection('to_visit_countries');
+  const visitedCountriesCollection = db.collection('visited_countries');
+
+  server.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
+});
