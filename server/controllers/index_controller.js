909d9c78a7753a7de96709776f2585b4c7ed0658
David <davidsanchezr81@gmail.com>
2018-05-01 11:12:03 +0100

    Created route to lists page

--- server/controllers/index_controller.js
+++ server/controllers/index_controller.js
@@ -3,13 +3,19 @@ const router = new express.Router();
 const createVisitedRouter = require('./visited_controller.js')
 const createToVisitRouter = require('./to_visit_controller.js')
 const createScheduleRouter =require('./schedule_controller.js')
-
+const createListRouter = require('./lists_controller.js')
+const path = require('path')
 
 const createIndexRouter = function(dbConnection){
 
   router.use('/visited', createVisitedRouter(dbConnection));
   router.use('/tovisit', createToVisitRouter(dbConnection));
   router.use('/schedule', createScheduleRouter(dbConnection));
+  router.use('/lists', createListRouter());
+
+  // router.get('/', function(req, res){
+  //   res.sendFile(path.join(__dirname,'../../client/public', 'index.html'))
+  // })
 
   return router;
 }
