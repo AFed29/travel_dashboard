e4aa227896ef060a9dea516ee5c26d1d897d92e1
David <davidsanchezr81@gmail.com>
2018-05-01 10:32:19 +0100

    Marker overlapping solved

--- client/src/models/to_visit.js
+++ client/src/models/to_visit.js
@@ -1,7 +1,7 @@
 
 const ToVisit = function(country) {
   this.name = country.name;
-  this.latlng = {lat: country.latlng[0], lng: country.latlng[1]};
+  this.latlng = {lat: country.latlng[0], lng: (country.latlng[1] + 0.5)};
   this.flagUrl = country.flag;
   this.capital = country.capital;
   this.currencies = country.currencies;
--- client/src/models/visited.js
+++ client/src/models/visited.js
@@ -1,6 +1,6 @@
 const Visited = function(country) {
   this.name = country.name;
-  this.latlng = {lat: country.latlng[0], lng: country.latlng[1]};
+  this.latlng = {lat: country.latlng[0], lng: (country.latlng[1] - 0.5)};
 }
 
 module.exports = Visited;
