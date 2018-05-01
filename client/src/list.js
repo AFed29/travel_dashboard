1263b21949e511146c41ce0f91b589b16cd35531
David <davidsanchezr81@gmail.com>
2018-05-01 13:16:25 +0100

    List page populated with visited / to visit countries data

---
+++ client/src/list.js
@@ -0,0 +1,22 @@
+const VistedView = require('./views/visited_view.js')
+const ToVisitView = require('./views/to_visit_view.js')
+const Request = require('./helpers/request.js')
+
+const toVisitView = new ToVisitView();
+const visitedView = new VistedView();
+
+const visitedRequest = new Request('http://localhost:3000/visited/');
+const toVisitRequest = new Request('http://localhost:3000/tovisit/');
+
+const listStart = function(){
+
+  toVisitRequest.get((toVisitCountries) => {
+    toVisitView.renderAll(toVisitCountries);
+  });
+
+  visitedRequest.get((visitedCountries) => {
+    visitedView.renderAll(visitedCountries);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', listStart);
